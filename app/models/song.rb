0375ef7694377ec1347b99772f10490525608d7c
Lauren McCoy <lmccoy8911@gmail.com>
2019-07-20 15:00:47 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -14,7 +14,7 @@ class Song < ActiveRecord::Base
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
     #binding.pry 
     Artist.find_or_create_by(name: "Drake")
-    
+    binding.pry 
     # self.artist.where(name: "Drake").first_or_create do |artist|
     #   artist.name = "Drake"
     #   end 
